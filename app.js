a67e812720b50ce5d1e435dbc6d3ae4bc009fbdf
damiga14 <davidamiga14@gmail.com>
2019-05-06 19:28:03 -0500

    estructura basica

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+
+var app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(7924, function()
+{
+    console.log("Server on port 7924")
+})
